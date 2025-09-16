2c1fdd5d30c071e0d74268aed2c690f329783cf4
mendana <diegodiazmenda@gmail.com>
2025-09-16 10:52:17 +0200

    feat: update dependencies and add new features
    - Added @astrojs/mdx and @astrojs/solid-js dependencies to package.json.
    - Included solid-js and vexflow for enhanced functionality.
    - Updated links in English and Spanish documentation for better navigation.
    - Removed outdated Spanish example guide.
    - Added custom CSS for centered tables.
    - Enhanced TypeScript configuration for Solid.js support.
    - Introduced a new Astro component for rendering musical scores using VexFlow.
    - Created a new introductory guide in Spanish covering music notation and basic concepts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,52 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mdx from '@astrojs/mdx';
+
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kromati-k.web.app',
-	integrations: [
-		starlight({
-			title: 'Kromati-K',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+	integrations: [starlight({
+		title: 'Kromati-K',
+		social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+		sidebar: [
+			{
+				label: 'Primeras nociones',
+				translations: {
+					en: 'Getting Started',
+					es: 'Primeras nociones',
 				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: [
-				'./src/styles/custom.css'
-			],
-			// logo: {
-			// 	src: '/logo.png',
-			// },
-			defaultLocale: 'es',
-			locales: {
-				es: {label: 'Español' },
-				en: { label: 'English'},
-			}
-		}),
+				items: [
+					{
+						label: 'Introducción',
+						translations: {
+							en: 'Introduction',
+							es: 'Introducción',
+						},
+						slug: 'primeras-nociones/introduccion',
+					},
+				],
+			},
+			{
+				label: 'Reference',
+				autogenerate: { directory: 'reference' },
+			},
+		],
+		customCss: [
+			'./src/styles/custom.css'
+		],
+		// logo: {
+		// 	src: '/logo.png',
+		// },
+		defaultLocale: 'es',
+		locales: {
+			es: { label: 'Español' },
+			en: { label: 'English' },
+		}
+	}),
+	mdx(),
+	solidJs()
 	],
 });
---
+++ src/content/docs/es/primeras-nociones/introduccion.md
@@ -0,0 +1,31 @@
+---
+title: Introducción
+description: Primeros pasos, notación musical y conceptos básicos empleados en la música moderna.
+---
+
+
+En esta sección, aprenderemos los conceptos básicos de la teoría musical y su notación. Estos fundamentos son esenciales para comprender cómo se estructura la música y cómo se pueden crear y analizar piezas musicales.
+
+## Notación musical
+La notación musical es un sistema de símbolos que representan los sonidos musicales. Estos símbolos incluyen notas, silencios, claves, compases y otros elementos que nos permiten leer y escribir música.
+
+### Nomenclatura de las notas
+En la música occidental, se presentan dos notaciones habituales para nombrar las notas musicales: la notación latina y la notación anglosajona:
+
+<table className="tabla-centrada">
+    <tbody>
+        <tr>
+            <td><strong>Nota</strong></td><td>La</td><td>Si</td><td>Do</td><td>Re</td><td>Mi</td><td>Fa</td><td>Sol</td>
+        </tr>
+        <tr>
+            <td><strong>Notación anglosajona</str</td><td>A</td><td>B</td><td>C</td><td>D</td><td>E</td><td>F</td><td>G</td>
+        </tr>
+    </tbody>
+</table>
+
+> Ambas notaciones son equivalentes y pueden usarse indistintamente según la preferencia personal o el contexto cultural. En este caso, preferimos usar la notación anglosajona por universalidad.
+
+### Pentagrama
+Es común emplear la representación gráfica de las notas en un **pentagrama**, que es un conjunto de cinco líneas horizontales paralelas donde se colocan las notas musicales. La posición de una nota en el pentagrama indica su altura tonal.
+
+<Partitura1 client:load />
